474f638311d975d6810fa2f2e65745aa93356e0e
Anatoliy Korotenko <tolik.korotenko@21gmail.com>
2025-08-21 17:03:23 +0300

    sol

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
 import React from 'react';
 
+type State = {
+  litters: string;
+};
+
+class KeyBord extends React.Component {
+  state: Readonly<State> = {
+    litters: '',
+  };
+
+  handleDocumentKeyBoard = (event: KeyboardEvent) => {
+    this.setState({ litters: event.key });
+  };
+
+  componentDidMount(): void {
+    window.addEventListener('keyup', this.handleDocumentKeyBoard);
+  }
+
+  componentWillUnmount(): void {
+    window.removeEventListener('keyup', this.handleDocumentKeyBoard);
+  }
+
+  render(): React.ReactNode {
+    const { litters } = this.state;
+
+    return (
+      <div>
+        {litters ? (
+          <p className="App__message">The last pressed key is [{litters}]</p>
+        ) : (
+          <p className="App__message">Nothing was pressed yet</p>
+        )}
+      </div>
+    );
+  }
+}
+
 export const App: React.FC = () => (
   <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
+    <KeyBord />
   </div>
 );
